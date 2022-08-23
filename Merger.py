4920fc85ab4fc92d2c21a6a40d72b153393635e6
ekenny456 <ekenny456@gmail.com>
2022-08-22 17:40:11 -0700

    Completed merger script
    - iterates through "images" folder appending each PDF to single file of inputted name
    
    - removes previous images from "images" folder leaving only combined PDF

---
+++ Merger.py
@@ -0,0 +1,16 @@
+import PyPDF2
+import os
+import sys
+
+os.chdir("images")
+
+name = input("File name of merged PDF: ") + ".pdf"
+
+fileMerger = PyPDF2.PdfFileMerger()
+
+for file in os.listdir(os.curdir):
+    if file.endswith(".pdf"):
+        fileMerger.append(file)
+        os.remove(file)
+
+fileMerger.write(name)
