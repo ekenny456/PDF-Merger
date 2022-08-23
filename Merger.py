96ca94be2609dd628c8e0d3aa2e84fbd5e7b00d1
ekenny456 <ekenny456@gmail.com>
2022-08-22 18:10:14 -0700

    Updated Documentation

--- Merger.py
+++ Merger.py
@@ -2,15 +2,15 @@ import PyPDF2
 import os
 import sys
 
-os.chdir("PDFs")
+os.chdir("PDFs")                                    # changes current directory to "PDFs" folder
 
-name = input("File name of merged PDF: ") + ".pdf"
+name = input("File name of merged PDF: ") + ".pdf"  # file name input
 
-fileMerger = PyPDF2.PdfFileMerger()
+fileMerger = PyPDF2.PdfFileMerger()                 # creates new PdfFileMerger object
 
-for file in os.listdir(os.curdir):
+for file in os.listdir(os.curdir):                  # iterates through files in current directory ("PDFs" folder)
     if file.endswith(".pdf"):
-        fileMerger.append(file)
-        os.remove(file)
+        fileMerger.append(file)                     # appends file to PdfFileMerger object
+        os.remove(file)                             # removes file from "PDFs" folder
 
-fileMerger.write(name)
+fileMerger.write(name)                              # writes PdfFileMerger object to PDF with inputted file name
